52c018ed2411e13290be08a4a12ea5203612ae37
shpark15 <shpark15@cafe24corp.com>
2022-08-17 09:47:48 +0900

    backend 프로젝트 세팅

---
+++ backend/src/main/java/com/festa/hack/kurly/KurlyApplication.java
@@ -0,0 +1,13 @@
+package com.festa.hack.kurly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurlyApplication.class, args);
+	}
+
+}
