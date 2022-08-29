25d7689a3aad9f360beb6de5f956240325f7d354
nacoon53 <nacoon53@gmail.com>
2022-08-29 22:49:03 +0900

    #84 feat: 로그인 토큰 만료 시 로그인창으로 이동하기

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -46,10 +46,31 @@ _axios.defaults.headers.common['Content-Type'] = "application/json";
 _axios.defaults.headers.common['Access-Control-Request-Methods'] = "GET, POST, PUT";
 
 _axios.interceptors.request.use((config) => {
+  //axios 요청 시 파라미터에 token 전달
   var token = VueCookie.get("token");
   _axios.defaults.headers.common['jwt-auth-token'] = token;
   return config;
 }, function (error) {
+  return Promise.reject(error);
+});
+
+_axios.interceptors.response.use((res) => {
+  return res;
+}, function (error) {
+  if(error.response.status == 403) {
+    VueCookie.delete("token");
+    _axios.defaults.headers.common['jwt-auth-token'] = '';
+
+    swal.fire({
+      title: `Session Expired`,
+      text: `세션이 만료되었습니다. 로그인을 다시 해주세요.`,
+      buttonsStyling: false,
+      confirmButtonClass: 'btn btn-warning',
+      icon: 'warning'
+    });
+
+    router.push('/login').catch(err => {});
+  }
   return Promise.reject(error);
 });
 
